616f4da24f926e5dd0a68f15ef28a3dda6ae2145
Francisco Serrano <fserrano@apple.com>
2023-08-28 17:59:23 -0700

    its squricle time

---
+++ script.js
@@ -0,0 +1,45 @@
+let circleSlider, squareSlider;
+let squareSize, circleDiameter;
+
+function setup() {
+  createCanvas(400, 400);
+
+  circleSlider = createSlider(10, 200, 100); // Slider for circle size
+  circleSlider.position(10, height + 10);
+
+  squareSlider = createSlider(10, 200, 100); // Slider for square size
+  squareSlider.position(10, height + 30);
+
+  textSize(16);
+}
+
+function draw() {
+  background(220);
+
+  let circleSize = circleSlider.value();
+  squareSize = squareSlider.value();
+  circleDiameter = circleSlider.value();
+
+  let squareX = (width - squareSize) / 2;
+  let squareY = (height - squareSize) / 2;
+  let circleX = width / 2;
+  let circleY = height / 2;
+
+  stroke(0);
+  noFill();
+
+  // Draw the square
+  rect(squareX, squareY, squareSize, squareSize);
+
+  // Draw the circle
+  ellipse(circleX, circleY, circleSize, circleSize);
+
+  fill(0);
+  noStroke();
+
+  // Display the square length and circle diameter
+  let infoText = `Square Length: ${squareSize}\nCircle Diameter: ${circleDiameter.toFixed(
+    2
+  )}`;
+  text(infoText, 10, height - 50);
+}
